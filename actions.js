85235d71342b067ea38577170d8de677655d092e
Max-Hi <levin.hirsch@web.de>
2024-05-13 18:07:02 +0200

    actions requirements added.

--- actions.js
+++ actions.js
@@ -7,3 +7,33 @@ export async function saveEventData(formdata) {
     const { resp } = await sql`INSERT INTO Events (Name, Date) VALUES (${data.name}, ${data.date})`
     console.log(resp)
 }
+
+export async function saveMemberData(formdata) {
+    const data = Object.fromEntries(Array.from(formdata.entries()).filter(([key, value]) => !key.includes("$ACTION_ID")))
+    const { resp } = await sql`INSERT INTO Events (Name, Date) VALUES (${data.name}, ${data.date})`
+    console.log(resp)
+}
+
+export async function deleteMemberData(formdata) { // formdata=ID:id
+    const data = Object.fromEntries(Array.from(formdata.entries()).filter(([key, value]) => !key.includes("$ACTION_ID")))
+    const { resp } = await sql`INSERT INTO Events (Name, Date) VALUES (${data.name}, ${data.date})`
+    console.log(resp)
+}
+
+export async function deleteEventData(formdata) {
+    const data = Object.fromEntries(Array.from(formdata.entries()).filter(([key, value]) => !key.includes("$ACTION_ID")))
+    const { resp } = await sql`INSERT INTO Events (Name, Date) VALUES (${data.name}, ${data.date})`
+    console.log(resp)
+}
+
+export async function editMemberData(formdata) {//formdata=ID:id, all other data where stuff that should not be changed is blank
+    const data = Object.fromEntries(Array.from(formdata.entries()).filter(([key, value]) => !key.includes("$ACTION_ID")))
+    const { resp } = await sql`INSERT INTO Events (Name, Date) VALUES (${data.name}, ${data.date})`
+    console.log(resp)
+}
+
+export async function editEventData(formdata) {
+    const data = Object.fromEntries(Array.from(formdata.entries()).filter(([key, value]) => !key.includes("$ACTION_ID")))
+    const { resp } = await sql`INSERT INTO Events (Name, Date) VALUES (${data.name}, ${data.date})`
+    console.log(resp)
+}
